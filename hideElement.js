782c1f4170ab1dec8764b965dc7350b4cfa0575b
Kai Waldrant <kai.waldrant@outlook.com>
2025-10-17 21:24:25 +0200

    fix popup and some margin for mobile

--- hideElement.js
+++ hideElement.js
@@ -2,22 +2,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Get the clickable element (for example, a button)
   const showButton = document.getElementById("shareShowButton");
-
   const hideButton = document.getElementById("shareHideButton");
 
 
   // Get the element you want to hide
-  const target = document.getElementById("footer");
   const popupTarget = document.getElementById("popup");
 
   // Add click event listener
   showButton.addEventListener("click", function () {
-    target.style.display = "none";
     popupTarget.style.display = "flex";
   });
 
     hideButton.addEventListener("click", function () {
-      popupTarget.style.display = "none";
-      target.style.removeProperty("display");
+      popupTarget.style.removeProperty("display");
     });
 });
