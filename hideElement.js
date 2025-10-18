d9f4180ff352118fdc4369afb5090ec20fa9f7b0
Kai Waldrant <kai.waldrant@outlook.com>
2025-10-18 22:43:54 +0200

    WIP using popover

--- hideElement.js
+++ hideElement.js
@@ -1,19 +1,19 @@
 // Wait until the page is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   // Get the clickable element (for example, a button)
-  const showButton = document.getElementById("shareShowButton");
+  // const showButton = document.getElementById("shareShowButton");
   const hideButton = document.getElementById("shareHideButton");
-
+  const popover = document.getElementById("popup");
 
   // Get the element you want to hide
   const popupTarget = document.getElementById("popup");
 
   // Add click event listener
-  showButton.addEventListener("click", function () {
-    popupTarget.style.display = "flex";
-  });
+  // showButton.addEventListener("click", function () {
+  //   popupTarget.style.display = "flex";
+  // });
 
-    hideButton.addEventListener("click", function () {
-      popupTarget.style.removeProperty("display");
-    });
+  hideButton.addEventListener("click", function () {
+    popover.hidePopover();
+  });
 });
