b0bd168b2709c3f2e91d7c5811f73a6e16395a60
Kai Waldrant <kai.waldrant@outlook.com>
2025-10-19 21:44:11 +0200

    fix popover

--- hideElement.js
+++ hideElement.js
@@ -1,19 +1,23 @@
 // Wait until the page is fully loaded
 document.addEventListener("DOMContentLoaded", function () {
   // Get the clickable element (for example, a button)
-  // const showButton = document.getElementById("shareShowButton");
+  const showButton = document.getElementById("shareShowButton");
+  const showButtonIcon = document.getElementById("shareShowButtonIcon");
   const hideButton = document.getElementById("shareHideButton");
   const popover = document.getElementById("popup");
 
-  // Get the element you want to hide
-  const popupTarget = document.getElementById("popup");
-
-  // Add click event listener
-  // showButton.addEventListener("click", function () {
-  //   popupTarget.style.display = "flex";
-  // });
 
   hideButton.addEventListener("click", function () {
     popover.hidePopover();
+  });
+
+  popover.addEventListener("beforetoggle", function () {
+    if (!showButton.style.backgroundColor) {
+      showButton.style.backgroundColor = "var(--color-grey-500)";
+      showButtonIcon.src = "/assets/images/icon-share-white.svg";
+    } else {
+      showButton.style.removeProperty("background-color");
+      showButtonIcon.src = "/assets/images/icon-share.svg";
+    }
   });
 });
